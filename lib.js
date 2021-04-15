24e87045a8ca88b8cef4d50fc976faf691d7c73b
Gus Diaz <gdiazbanuelos@gmail.com>
2021-04-15 11:46:07 -0500

    Updated the getLDistance and removed test functions

--- lib.js
+++ lib.js
@@ -8,35 +8,15 @@ const fs = require('fs');
 
 /* Levenshtein function to check guess to answer */
 /* Correct guess: true, Incorrect: false, Close: -1*/
-function getLDistance(guess, target) {
-  guess = guess.toLowerCase();
-  target = target.toLowerCase();
-  let ld = levenshtein(guess, target);
-  if (ld == 0) { return true; }
-  else {
-    let newLD = levenshtein(pluralize(guess), pluralize(target));
-    if (newLD == 0) { return true; }
-    else if (newLD <= 2) { return -1; }
-    else { return false; }
-  }
-}
-
-
-function test_getLDistance() {
-  let guess = 'firETruCks'
-  let target = 'firetruck'
-  console.log(`Guess: ${guess}, Target: "${target}`)
-  console.log(getLDistance(guess, target));
-  console.log('---');
-  guess = 'cars'
-  target = 'firetruck'
-  console.log(`Guess: ${guess}, Target: "${target}`)
-  console.log(getLDistance(guess, target));
-  console.log('---');
-  guess = 'cat'
-  target = 'bats'
-  console.log(`Guess: ${guess}, Target: "${target}`)
-  console.log(getLDistance(guess, target));
+function getLDistance(guess, target){
+    guess = guess.toLowerCase();
+    target = target.toLowerCase();
+    var ld = levenshtein(guess, target);
+    if(ld == 0){return ld;}
+    else{
+        var newLD = levenshtein(pluralize(guess), pluralize(target));
+        return newLD;
+    }
 }
 
 /* Read CSV file and turn into JSON object */
@@ -48,13 +28,6 @@ function readCSV(filePath) {
     .map(e => e.split(',').map(e => e.trim()));
   return data[0];
 }
-
-
-function test_readCSV() {
-  let a = readCSV('compounds.csv');
-  console.log(a);
-}
-
 
 /* General math/data structure functions */
 function cloneArray(source, dest) {
